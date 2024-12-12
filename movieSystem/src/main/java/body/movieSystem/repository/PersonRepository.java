94c3a474a1bcfbe6bdfea8938c782eb43e848742
DESKTOP-6VU63T9\ernur <ernur>
2024-12-12 21:28:53 +0300

    Add repository layer for data persistence

---
+++ movieSystem/src/main/java/body/movieSystem/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package body.movieSystem.repository;
+
+import body.movieSystem.entity.Revenue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository
+    extends JpaRepository<Revenue, Integer>, JpaSpecificationExecutor<Revenue> {}
